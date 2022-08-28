3bf3a2fd07caab40c3d4673f40168c2586e690d8
Deckason <chibuikemchidera@gmail.com>
2022-08-28 03:25:57 +0100

    Login system using react useForm and yup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver} from '@hookform/resolvers/yup'
+import * as yup from "yup";
 
 function App() {
+
+  const schema = yup.object().shape({
+    username: yup.string().min(3).required("Input required"),
+    email: yup.string().email().required("Input required"),
+    age: yup.number().positive().min(18).integer().required("Input required"),
+    password: yup.string().min(8).required("Input required"),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required("Input required")
+  });
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function formSubmitted(data) {
+    console.log(data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit(formSubmitted)}>
+          <div className='input'>
+            <label htmlFor="username">Username</label>
+            <input type="text" placeholder='Enter Username' {...register("username")}/>
+            <small>{errors.username?.message}</small>
+          </div>
+          <div className='input'>
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder='Enter Email' {...register("email")}/>
+            <small>{errors.email?.message}</small>
+          </div>
+          <div className='input'>
+            <label htmlFor="age">Age</label>
+            <input type="number" placeholder='Enter Age' {...register("age")}/>
+            <small>{errors.age?.message}</small>
+          </div>
+          <div className='input'>
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='Enter Password' autoComplete='password' {...register("password")}/>
+            <small>{errors.password?.message}</small>
+          </div>
+          <div className='input'>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" placeholder='Confirm Password' autoComplete='password' {...register("confirmPassword")}/>
+            <small>{errors.confirmPassword?.message}</small>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
